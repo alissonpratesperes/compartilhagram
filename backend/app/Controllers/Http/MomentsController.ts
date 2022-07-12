3927f402bf47fcc6b265a531b3ed1cc2a98a6d31
alissonpratesperes <a.pratesperes@gmail.com>
2022-07-12 19:41:29 -0300

    ✨ Load dos Comments nos Moments no Controller - "MomentsController" e finalização da API da Aplicação.
    Na Função Index, atualizado o que antes era "Moment.all();" para "Moment.query().preload('comments');" assim na Listagem de Todos os Moments, trazendo um Array de Comments para cada um deles, exibindo os Comments vinculados a cada um;
    Na Função Show, inserido "moment.load('comments');" que também faz o papel do Método ".preload('comments');" na Listagem de Todos os Moments, só que aqui também irá trazer um Array de Comments para cada Moment carregado, exibindo os Comments vinculados nesse Moment em específico;
    Também finalizado o desenvolvimento da API da Aplicação: Compartilhagram.

--- backend/app/Controllers/Http/MomentsController.ts
+++ backend/app/Controllers/Http/MomentsController.ts
@@ -4,99 +4,101 @@ import { v4 as uuidv4 } from 'uuid';
 
 import Moment from 'App/Models/Moment';
 
-export default class MomentsController {
+  export default class MomentsController {
 
-  private validationOptions = {
-    types: ['image'],
-    size: '20mb'
-  };
+    private validationOptions = {
+      types: ['image'],
+      size: '20mb'
+    };
 
-    public async store({ request, response }: HttpContextContract) {
-      const body = request.body();
-      const image = request.file('image', this.validationOptions);
+      public async store({ request, response }: HttpContextContract) {
+        const body = request.body();
+        const image = request.file('image', this.validationOptions);
 
-        if(image) {
-          const imageName = `${uuidv4()}.${image.extname}`;
+          if(image) {
+            const imageName = `${uuidv4()}.${image.extname}`;
 
-            await image.move(Application.tmpPath('uploads'), {
-              name: imageName
-            });
+              await image.move(Application.tmpPath('uploads'), {
+                name: imageName
+              });
 
-              body.image = imageName;
-        }
-
-      const moment = await Moment.create(body);
-
-        response.status(201);
-
-          return {
-            message: "Moment created successfully!",
-            data: moment
-          }
-    }
-
-    public async index({ response }: HttpContextContract) {
-      const moments = await Moment.all();
-
-        response.status(200);
-
-          return {
-            data: moments
-          }
-    }
-
-    public async show({ response, params }: HttpContextContract) {
-      const moment = await Moment.findOrFail(params.id);
-
-        response.status(200);
-
-          return {
-            data: moment
-          }
-    }
-
-    public async update({ request, response, params }: HttpContextContract) {
-      const body = request.body();
-      const moment = await Moment.findOrFail(params.id);
-
-        moment.title = body.title;
-        moment.description= body.description;
-
-          if(moment.image != body.image || !moment.image) {
-            const image = request.file('image', this.validationOptions);
-
-              if(image) {
-                const imageName = `${uuidv4()}.${image.extname}`;
-
-                  await image.move(Application.tmpPath('uploads'), {
-                    name: imageName
-                  });
-
-                    moment.image = imageName;
-              }
+                body.image = imageName;
           }
 
-            await moment.save();
-
-              response.status(201);
-
-                return {
-                  message: "Moment updated successfully!",
-                  data: moment
-                }
-    }
-
-    public async destroy({ response, params }: HttpContextContract) {
-      const moment = await Moment.findOrFail(params.id);
-
-        await moment.delete();
+        const moment = await Moment.create(body);
 
           response.status(201);
 
             return {
-              message: "Moment deleted successfully!",
+              message: "Moment created successfully!",
               data: moment
             }
-    }
+      }
 
-}
+      public async index({ response }: HttpContextContract) {
+        const moments = await Moment.query().preload('comments');
+
+          response.status(200);
+
+            return {
+              data: moments
+            }
+      }
+
+      public async show({ response, params }: HttpContextContract) {
+        const moment = await Moment.findOrFail(params.id);
+
+          await moment.load('comments');
+
+            response.status(200);
+
+              return {
+                data: moment
+              }
+      }
+
+      public async update({ request, response, params }: HttpContextContract) {
+        const body = request.body();
+        const moment = await Moment.findOrFail(params.id);
+
+          moment.title = body.title;
+          moment.description= body.description;
+
+            if(moment.image != body.image || !moment.image) {
+              const image = request.file('image', this.validationOptions);
+
+                if(image) {
+                  const imageName = `${uuidv4()}.${image.extname}`;
+
+                    await image.move(Application.tmpPath('uploads'), {
+                      name: imageName
+                    });
+
+                      moment.image = imageName;
+                }
+            }
+
+              await moment.save();
+
+                response.status(201);
+
+                  return {
+                    message: "Moment updated successfully!",
+                    data: moment
+                  }
+      }
+
+      public async destroy({ response, params }: HttpContextContract) {
+        const moment = await Moment.findOrFail(params.id);
+
+          await moment.delete();
+
+            response.status(201);
+
+              return {
+                message: "Moment deleted successfully!",
+                data: moment
+              }
+      }
+
+  }
