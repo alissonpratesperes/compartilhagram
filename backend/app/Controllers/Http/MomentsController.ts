7c6435fd644077ee60cd0e10f9da05a248624e01
alissonpratesperes <a.pratesperes@gmail.com>
2022-07-12 00:26:12 -0300

    :necktie: Desenvolvimento do Método "store" do "MomentsController" no Back-End da Aplicação.
    Coletadas as variáveis Request e Response utilizando a Desestruturação, do "HttpContextContract" (que contém todos os dados da Requisição HTTP realizada, como Query Params/Body, por exemplo);
    Utilizado a Model do Moment para executar o Método ".create" do Lucid ORM, para assim gravar no Banco de Dados o "body" da Requisição (que é enviado como parâmetro/argumento da função) com as informações do Moment que está sendo cadastrado (como: title, description e image - que será desenvolvido o upload posteriormente);
    Retorno de HTTP Status Code 201, e retorno padronizado dessa API com "message" e "data", onde "data" retornará o Moment criado no Banco de Dados, no Back-End da Aplicação.

--- backend/app/Controllers/Http/MomentsController.ts
+++ backend/app/Controllers/Http/MomentsController.ts
@@ -1,11 +1,19 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+
+import Moment from 'App/Models/Moment';
 
 export default class MomentsController {
 
-  public async store() {
-    return {
-      msg: "Deu certo!"
-    }
+  public async store({ request, response }: HttpContextContract) {
+    const body = request.body();
+    const moment = await Moment.create(body);
+
+      response.status(201);
+
+        return {
+          message: "Moment created successfully!",
+          data: moment
+        }
   }
 
 }
