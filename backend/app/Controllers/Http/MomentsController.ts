6622f8fdeef9158bfa5f40ab7f026d7b46bcae00
alissonpratesperes <a.pratesperes@gmail.com>
2022-07-12 12:50:42 -0300

    :sparkles: Upload de MomentImages no Controller - "MomentsController" no Back-End da Aplicação.
    Instalada nova Dependência - uuid que será responsável por gerar String Names para cada Moment Image que tiver Upload na Aplicação;
    Implementados dois novos Imports: o "uuid" que irá gerar Strings específicas para cada Moment Image e também o "Application" que irá lidar com o Store de cada Moment Image no Diretório: "backend/tmp/uploads";
    Configurado o Método Privado com as "validationOptions" para tratar tipos de arquivo para somente Imagens e também para tratar o tamanho de cada uma, na qual foi configurado o limite de até "20mb", no Back-End da Aplicação.

--- backend/app/Controllers/Http/MomentsController.ts
+++ backend/app/Controllers/Http/MomentsController.ts
@@ -1,19 +1,37 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import Application from '@ioc:Adonis/Core/Application';
+import { v4 as uuidv4 } from 'uuid';
 
 import Moment from 'App/Models/Moment';
 
 export default class MomentsController {
+  private validationOptions = {
+    types: ['image'],
+    size: '20mb'
+  };
 
-  public async store({ request, response }: HttpContextContract) {
-    const body = request.body();
-    const moment = await Moment.create(body);
+    public async store({ request, response }: HttpContextContract) {
+      const body = request.body();
+      const image = request.file('image', this.validationOptions);
 
-      response.status(201);
+        if(image) {
+          const imageName = `${uuidv4()}.${image.extname}`;
 
-        return {
-          message: "Moment created successfully!",
-          data: moment
+            await image.move(Application.tmpPath('uploads'), {
+              name: imageName
+            });
+
+              body.image = imageName;
         }
-  }
+
+      const moment = await Moment.create(body);
+
+        response.status(201);
+
+          return {
+            message: "Moment created successfully!",
+            data: moment
+          }
+    }
 
 }
