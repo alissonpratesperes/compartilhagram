de0343e214cb752680e208140b0f236ba33a1eb2
alissonpratesperes <a.pratesperes@gmail.com>
2022-07-14 20:59:35 -0300

    :art: Configuração de Rotas e Navegação no Front-End da Aplicação.
    Configuradas as Rotas para as Páginas About e Home, bem como adicionada a Navegação para estas no Header Component;
    Seguindo o padrão de desenvolvimento do Curso, no App Component, adicionados os Components do Header e Footer (referentes às respectivas Páginas) e adicionado o Router Outlet Component que irá realizar o Roteamento durante a navegação entre as Páginas da Aplicação;
    Desenvolvida a Estilização CSS global, de acordo com o Curso, porém ao Desenvolvimento Finalizado, a Aplicação - Compartilhagram irá sofrer um re-branding para tornar o projeto único.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+  const routes: Routes = [
+    {
+      path: '',
+        component: HomeComponent
+    },
+    {
+      path: 'about',
+        component: AboutComponent
+    }
+  ];
+
+    @NgModule({
+      imports: [RouterModule.forRoot(routes)],
+      exports: [RouterModule]
+    })
+
+      export class AppRoutingModule { }
