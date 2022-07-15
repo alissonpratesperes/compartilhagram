b9fdf46c512bac797502f73604bed2a268bd4e38
alissonpratesperes <a.pratesperes@gmail.com>
2022-07-15 14:11:14 -0300

    :boom: Implementação de Formulário com Reactive Forms no Front-End da Aplicação.
    Implementada a Validação de Formulário, que servirá tanto para o Cadastro de Moments quanto para a Edição de Moments, utilizando o Reactive Forms do Angular, no Front-End da Aplicação.

--- frontend/src/app/components/momentform/momentform.component.ts
+++ frontend/src/app/components/momentform/momentform.component.ts
@@ -1,17 +1,43 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
-@Component({
-  selector: 'app-momentform',
-  templateUrl: './momentform.component.html',
-  styleUrls: ['./momentform.component.css']
-})
-export class MomentformComponent implements OnInit {
+  @Component({
+    selector: 'app-momentform',
+    templateUrl: './momentform.component.html',
+    styleUrls: ['./momentform.component.css']
+  })
 
-  @Input() buttonText!: string;
+    export class MomentformComponent implements OnInit {
 
-  constructor() { }
+      @Input() buttonText!: string;
 
-  ngOnInit(): void {
-  }
+        momentForm!: FormGroup;
 
-}
+          constructor() { }
+
+            ngOnInit(): void {
+              this.momentForm = new FormGroup({
+                id: new FormControl(''),
+                title: new FormControl('', [ Validators.required ]),
+                description: new FormControl('', [ Validators.required ]),
+                image: new FormControl('')
+              });
+            }
+
+              get title() {
+                return this.momentForm.get('title')!;
+              }
+
+              get description() {
+                return this.momentForm.get('description')!;
+              }
+
+                submit() {
+                  if(this.momentForm.invalid) {
+                    return;
+                  }
+
+                    console.log('Enviou formulário.');
+                }
+
+    }
