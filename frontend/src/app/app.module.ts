84340bfc9eb2aaad6e055db1546bde101f57c76b
alissonpratesperes <a.pratesperes@gmail.com>
2022-07-15 13:27:22 -0300

    :sparkles: Importação de Módulos e criaçao de Interface - "Moment" no Front-End da Aplicação.
    Importados, no App Module, os módulos de Http e Formulários para o início da implementação da comunicação do Front-End com o Back-End;
    Criado novo Diretório "interfaces" que conterá as Interfaces para as Requisições da Aplicação, inicialmente contendo apenas a Interface - "Moment" que contém todas as informações de acordo com o Back-End, do Moment, no Front-End da Aplicação.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,21 +12,25 @@ import { HomeComponent } from './pages/home/home.component';
 import { NewmomentComponent } from './pages/newmoment/newmoment.component';
 import { MomentformComponent } from './components/momentform/momentform.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    AboutComponent,
-    HomeComponent,
-    NewmomentComponent,
-    MomentformComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+  @NgModule({
+    declarations: [
+      AppComponent,
+      HeaderComponent,
+      FooterComponent,
+      AboutComponent,
+      HomeComponent,
+      NewmomentComponent,
+      MomentformComponent
+    ],
+    imports: [
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule,
+      FormsModule,
+      ReactiveFormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+  })
+
+    export class AppModule { }
